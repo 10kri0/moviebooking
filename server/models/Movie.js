f0fdbfcba5b3b8556a6ba7a85e84f3011fbf9fa0
krish patel <114717796+10kri0@users.noreply.github.com>
2025-04-06 09:34:13 +0530

    Add files via upload

--- server/models/Movie.js
+++ server/models/Movie.js
@@ -1,52 +1,49 @@
 const mongoose = require('mongoose')
 
 const movieSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, 'Please add a movie name'],
-      trim: true
-    },
-    length: {
-      type: Number,
-      required: [true, 'Please add a movie length']
-    },
-    img: {
-      type: String,
-      required: [true, 'Please add a movie img'],
-      trim: true
-    },
-    description: {
-      type: String,
-      required: [true, 'Please add a description']
-    },
-    rating: {
-      type: Number,
-      required: [true, 'Please add a rating'],
-      min: [0, 'Rating must be at least 0'],
-      max: [10, 'Rating cannot exceed 10']
-    },
-    genre: {
-      type: String,
-      required: [true, 'Please add a genre'],
-      enum: [
-        'Action',
-        'Comedy',
-        'Drama',
-        'Horror',
-        'Sci-Fi',
-        'Thriller',
-        'Romance',
-        'Fantasy',
-        'Animation',
-        'Documentary'
-      ],
-      default: 'Drama'
-    }
-  },
-  { timestamps: true }
-)
 
+	{
+		name: {
+			type: String,
+			required: [true, 'Please add a movie name'],
+			trim: true
+		},
+		length: {
+			type: Number,
+			required: [true, 'Please add a movie length']
+		},
+		img: {
+			type: String,
+			required: [true, 'Please add a movie img'],
+			trim: true
+		},
+		description: {
+			type: String,
+			required: [true, 'Please add a description']
+		},
+		genre: {
+			type: String,
+			required: [true, 'Please add a genre']
+		},
+		rating: {
+			type: Number,
+			required: [true, 'Please add a rating']
+		},
+		language: {
+			type: String,
+			required: [true, 'Please add a language'],
+			enum: ['English', 'Hindi', 'Tamil', 'Telugu', 'Malayalam', 'Kannada', 'Other'],
+			default: 'English'
+		},
+		showType: {
+			type: String,
+			required: [true, 'Please add a show type'],
+			enum: ['2D', '3D', 'IMAX', '4DX'],
+			default: '2D'
+		}
+	},
+	{ timestamps: true }
+)
 movieSchema.pre('deleteOne', { document: true, query: true }, async function (next) {
   const movieId = this._id
   const showtimes = await this.model('Showtime').find({ movie: movieId })
